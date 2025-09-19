c3a3df536a5b20e9c1cb0f3e7cce767a6eacb82d
NyoLaeMay <nyolaemaynyolaemay@gmail.com>
2025-09-19 12:42:05 +0700

    feat: Why Dine With Us Data Only added

---
+++ src/data/whyUs.ts
@@ -0,0 +1,22 @@
+import { WhyUs } from "@/types/content";
+
+export const whyUsData: WhyUs[] = [
+  {
+    id: "1",
+    title: "Fresh locally sourced ingredients",
+    description:
+      "At Bon Appétit, we embrace the beauty of fresh, locally sourced ingredients. Our culinary team carefully chooses seasonal produce and artisanal goods from nearby farms, ensuring each dish is rich in flavor and authenticity.  Join us in celebrating the true flavors of our community!",
+  },
+  {
+    id: "2",
+    title: "Experienced Chefs, Exceptional Taste",
+    description:
+      "At Bon Appétit, our culinary team is dedicated to crafting unforgettable dining experiences. Each dish is a testament to our chefs' expertise and passion, showcasing a blend of traditional techniques and innovative flavors. We take pride in using high-quality ingredients, ensuring that every bite is a celebration of taste and artistry. Join us to indulge in a meal that reflects our commitment to excellence and creativity.",
+  },
+  {
+    id: "3",
+    title: "Fresh locally sourced ingredients",
+    description:
+      "At Bon Appétit, we embrace the beauty of fresh, locally sourced ingredients. Our culinary team carefully chooses seasonal produce and artisanal goods from nearby farms, ensuring each dish is rich in flavor and authenticity.  Join us in celebrating the true flavors of our community!",
+  },
+];
