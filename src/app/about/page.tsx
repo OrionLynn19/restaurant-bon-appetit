2f6b13b3cf6e162ff433d102ab79eeac920a3ad1
NyoLaeMay <nyolaemaynyolaemay@gmail.com>
2025-09-19 15:48:19 +0700

    feat:whyDineWithUs on AboutUs Page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,12 @@
-export default function About(){
-    return(
-        <div className="p-10 text-center text-3xl text-orange-500">This is About</div>
-    );
+import WhyUs from "@/components/WhyUs";
+
+export default function About() {
+  return (
+    <>
+      <div className="p-10 text-center text-3xl text-orange-500">
+        This is About
+      </div>
+      <WhyUs />
+    </>
+  );
 }
