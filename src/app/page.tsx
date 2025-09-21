ef80235f1aeb7901cfb10582f3f1ab56b0a7e80c
Thidar Aung <thidaraung00iii@gmail.com>
2025-09-22 02:08:18 +0700

    feat:welcomesection(home)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import HomeWelcomeSection from "./home-welcome-section";
 
 export default function Home() {
   return (
-    <div>
-      <div className="p-10 text-center text-3xl text-orange-500">This is Home Page</div>
-    </div>
+    <>
+      <div>
+        <div className="p-10 text-center text-3xl text-orange-500">This is Home Page</div>
+      </div>
+      <div><HomeWelcomeSection /></div>
+    </>
   );
 }
