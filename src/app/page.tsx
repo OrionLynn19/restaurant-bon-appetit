51667738165b29849760bc287d960777aa173ca5
Thidar Aung <thidaraung00iii@gmail.com>
2025-09-24 02:07:57 +0700

    feat:testimonial(home)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import HomeTestimonial from "./home-testimonial";
 
 export default function Home() {
   return (
+    <>
     <div>
       <div className="p-10 text-center text-3xl text-orange-500">This is Home Page</div>
     </div>
+    <div><HomeTestimonial/></div>
+    </>
   );
 }
