4349f96d57ec9c8ff3074a7c215f0612056b16b1
asett12 <158172308+asett12@users.noreply.github.com>
2025-09-25 14:21:15 +0700

    feat: build discount header with white time pills and orange labels

---
+++ src/components/MenuDetail.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MenuDetail() {
+  return (
+    <div className="min-h-screen bg-[#FFF7E9] text-[#073027]">
+      <div className="mx-auto max-w-6xl px-6 py-10">Menu Detail</div>
+    </div>
+  );
+}
