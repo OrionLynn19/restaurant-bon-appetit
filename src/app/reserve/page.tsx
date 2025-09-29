6759f299573d8070cbe95c0d852c15c8cefedf44
Lynn Pyae Hmue <orionlynn@Lynns-MacBook-Air-5.local>
2025-09-29 16:59:49 +0700

    confirm box developed

--- src/app/reserve/page.tsx
+++ src/app/reserve/page.tsx
@@ -1,5 +1,10 @@
+import ConfirmBox from "@/components/confirmBox";
+
 export default function Reserve(){
     return(
+        <>
         <div className="p-10 text-center text-3xl text-orange-500">This is Booking Page</div>
+        <ConfirmBox/>
+        </>
     );
 }
