97905a9f90266243b1fd2077f2745c8c147459b6
NyoLaeMay <nyolaemaynyolaemay@gmail.com>
2025-10-08 20:09:20 +0700

    feat:deliOption(cart)

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,15 @@
+"use client";
+import React from "react";
 import CartScreen from "@/components/CartScreen";
+import DeliveryOption from "@/components/DeliveryOption";
 
 export default function CartPage() {
-  return <CartScreen />;
+  return (
+    <div className="min-h-screen bg-[#FFFAEC]">
+      <div className="mx-auto w-full space-y-12 gap-[48px] md:gap-[96px] px-4 py-12 md:px-16 md:py-24">
+        <CartScreen />
+        <DeliveryOption />
+      </div>
+    </div>
+  );
 }
