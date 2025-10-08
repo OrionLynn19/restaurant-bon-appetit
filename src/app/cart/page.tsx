617406326bebc4c57a8d8735d30cb51bd900be20
NyoLaeMay <nyolaemaynyolaemay@gmail.com>
2025-10-08 20:39:03 +0700

    fix:gap between each section

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -5,8 +5,8 @@ import DeliveryOption from "@/components/DeliveryOption";
 
 export default function CartPage() {
   return (
-    <div className="min-h-screen bg-[#FFFAEC]">
-      <div className="mx-auto w-full space-y-12 gap-[48px] md:gap-[96px] px-4 py-12 md:px-16 md:py-24">
+    <div className="min-h-screen bg-[#FFFAEC] px-4 py-12 md:px-16 md:py-24">
+      <div className="mx-auto w-full space-y-12 md:space-y-24">
         <CartScreen />
         <DeliveryOption />
       </div>
