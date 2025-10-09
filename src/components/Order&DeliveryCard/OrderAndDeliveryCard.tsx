06ef48b732188a9ba1ac4df8e331526266c2c00f
asett12 <158172308+asett12@users.noreply.github.com>
2025-10-09 12:20:41 +0700

    added boilerplate for deliverydetails

---
+++ src/components/Order&DeliveryCard/OrderAndDeliveryCard.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+
+export default function OrderAndDeliveryCard() {
+  return (
+    <div className="rounded-xl border border-[#E4E4E4] bg-white">
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        {/* OrderDetails goes here */}
+        <div className="p-5 md:p-6 lg:p-7"> {/* replace with your DeliveryDetails */}
+          {/* DeliveryDetails goes here */}
+        </div>
+      </div>
+    </div>
+  );
+}
